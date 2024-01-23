84787f9ed7534e196990dbab1b2e0a6ad9b5e2d9
jedi <coding.jedi.knight@gmail.com>
2024-01-23 15:41:46 +0300

    (add)[config]{logic}

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idleTimeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable is not set")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error opening config file: %s", err)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, cfg); err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+
+	return &cfg
+}
